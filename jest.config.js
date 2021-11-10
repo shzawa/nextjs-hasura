e5d3070cd8fc5dd233b6f838c574335b461004b2
shzawa <ozawa.shotaro0114@gmail.com>
2021-11-10 22:08:53 +0900

    add: swc

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+const config = {
+  testEnvironment: "jsdom",
+  testMatch: ["**/*.test.ts", "**/*.test.tsx"],
+  "setupFilesAfterEnv": ["./jest.setup.js"],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/.next/",
+  ],
+  moduleNameMapper: {
+    "^~/(.*)": "<rootDir>/src/$1"
+  },
+  transformIgnorePatterns: ["/node_modules/"],
+  transform: {
+    ".+\\.(t|j)sx?$": [
+      "@swc/jest",
+      {
+        sourceMaps: true,
+        module: {
+          type: "commonjs",
+        },
+        jsc: {
+          parser: {
+            syntax: "typescript",
+            tsx: true,
+          },
+          transform: {
+            react: {
+              runtime: "automatic",
+            },
+          },
+        },
+      },
+    ],
+  },
+};
+
+module.exports = config;
